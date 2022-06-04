7566e6a90398a123fe999fc13414d64433d0402d
pdhende <pdhendework@gmail.com>
2022-06-04 10:17:26 -0400

    Updated package.json

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-// import SearchBooks from './pages/SearchBooks';
-// import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 // The apollo client provides authentication by using the following 'Header' method
